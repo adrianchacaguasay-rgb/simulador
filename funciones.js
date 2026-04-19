984fd98ea81cec1a94231ed7e9f0b386bccef72c
adrianchacaguasay-rgb <adrianchacaguasay@gmail.com>
2026-04-19 14:52:29 -0500

    Se agrego el calculo del interes y su visualizacion

--- funciones.js
+++ funciones.js
@@ -1,19 +1,39 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function texto(id, vari) {
+    let texto1 = document.getElementById(id);
+    texto1.innerText = "USD " + vari.toFixed(2);
+}
+
+function extraer(id) {
+    let text = parseFloat(document.getElementById(id).value);
+    return text;
+}
+
 function calcularDisponible(ingresos, egresos) {
     let disponible;
     disponible = ingresos - egresos;
-    if (disponible<0){  
+    if (disponible < 0) {
         return 0;
     }
     return disponible;
 }
-function capacidadDePago(montoDisponible){
+
+function capacidadDePago(montoDisponible) {
     let capacidad;
-    capacidad=montoDisponible/2;
+    capacidad = montoDisponible * 0.5;
     return capacidad;
 }
 
-function texto (id,vari){
-    let texto1 = document.getElementById(id);
-    texto1.innerText= "USD " + vari.toFixed(2);    
+function calcularInteresSimple(monto, tasa, plazoAnios) {
+    let interes = plazoAnios * monto * (tasa / 100);
+    return interes;
 }
+
+
+
+
+
+
+
+
+
--- simulador.js
+++ simulador.js
@@ -1,11 +1,19 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
 function calcular() {
-    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
-    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    let ingresos = extraer("txtIngresos");
+    let egresos = extraer("txtEgresos");
 
     let disponible = calcularDisponible(ingresos, egresos);
     texto("spnDisponible", disponible);
 
-    let capacidadPago= capacidadDePago(disponible);
-    texto("spnCapacidadPago",capacidadPago);
+    let capacidadPago = capacidadDePago(disponible);
+    texto("spnCapacidadPago", capacidadPago);
+
+    //calculo del interes
+    let monto = extraer("txtMonto");
+    let plazoAnios = extraer("txtPlazo");
+    let tasa = extraer("txtTasaInteres");
+
+    let valorInteres = calcularInteresSimple(monto, tasa, plazoAnios);
+    texto("spnInteresPagar", valorInteres)
 }
